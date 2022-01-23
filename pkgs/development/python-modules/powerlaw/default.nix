1444982f16c4a027aa20b77e9edc0f747fac0f34
nialov <nikolasovaskainen@gmail.com>
2022-01-23 15:56:47 +0200

    add information on version

--- pkgs/development/python-modules/powerlaw/default.nix
+++ pkgs/development/python-modules/powerlaw/default.nix
@@ -8,6 +8,8 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "jeffalstott";
     repo = "powerlaw";
+    # The version at this rev should be aligned to the one at pypi according
+    # to the commit message.
     rev = "6732699d790edbe27c2790bf22c3ef7355d2b07e";
     sha256 = "sha256-x3jXk+xOQpIeEGlzYqNwuZNPpkesF0IOX8gUhhwHk5Q=";
   };
